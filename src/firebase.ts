fb1c2c8e721ec89ae2e9ea2ab22c6d1b2a986823
mandoo8872 <166672756+mandoo8872@users.noreply.github.com>
2025-06-02 16:59:59 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,7 +6,7 @@ import { getDatabase, ref, onValue, set, off, DatabaseReference, DataSnapshot } 
 const firebaseConfig = {
   apiKey: "AIzaSyBF1sBC8tegqpQzlhZDkfRyCkG1N-RqHZM",
   authDomain: "board6-a2c5a.firebaseapp.com",
-  databaseURL: "https://board6-a2c5a-default-rtdb.firebaseio.com/",
+  databaseURL: "https://board6-a2c5a-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "board6-a2c5a",
   storageBucket: "board6-a2c5a.appspot.com",
   messagingSenderId: "192957529739",
